4c954f7b6564ccce3a64a9d2af3b8b93d577d9cc
Droy40 <dustindelroy@gmail.com>
2024-04-06 18:38:10 +0700

    commit

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Class
+namespace ClassLibrary
 {
-    public class Koneksi
+    public class Class1
     {
-        private MySqlConnection koneksiDB;
     }
 }
---
+++ ClassLibrary/Koneksi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+
+namespace ClassLibrary
+{
+    public class Koneksi
+    {
+        private MySqlConnection koneksiDB;
+
+        public Koneksi()
+        {
+            Configuration myConf = ConfigurationSettings.
+        }
+    }
+}
