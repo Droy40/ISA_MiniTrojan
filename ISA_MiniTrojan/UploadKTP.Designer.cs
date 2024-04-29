710aa0e5d3de643100fe6410e0c2f673bd655135
Salman Alfarizi <salmanlfaa11@gmail.com>
2024-04-29 19:36:48 +0700

    Stegano Upload KTP

--- ISA_MiniTrojan/UploadKTP.Designer.cs
+++ ISA_MiniTrojan/UploadKTP.Designer.cs
@@ -34,14 +34,16 @@
             this.buttonOpenFile = new System.Windows.Forms.Button();
             this.buttonEncode = new System.Windows.Forms.Button();
             this.buttonDecode = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKtp)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxKtp
             // 
-            this.pictureBoxKtp.Location = new System.Drawing.Point(48, 12);
+            this.pictureBoxKtp.Location = new System.Drawing.Point(64, 15);
+            this.pictureBoxKtp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBoxKtp.Name = "pictureBoxKtp";
-            this.pictureBoxKtp.Size = new System.Drawing.Size(357, 185);
+            this.pictureBoxKtp.Size = new System.Drawing.Size(476, 228);
             this.pictureBoxKtp.TabIndex = 44;
             this.pictureBoxKtp.TabStop = false;
             // 
@@ -51,19 +53,20 @@
             this.labelNoKTP.BackColor = System.Drawing.Color.Transparent;
             this.labelNoKTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelNoKTP.ForeColor = System.Drawing.Color.White;
-            this.labelNoKTP.Location = new System.Drawing.Point(12, 274);
+            this.labelNoKTP.Location = new System.Drawing.Point(16, 337);
+            this.labelNoKTP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelNoKTP.Name = "labelNoKTP";
-            this.labelNoKTP.Size = new System.Drawing.Size(83, 20);
+            this.labelNoKTP.Size = new System.Drawing.Size(107, 25);
             this.labelNoKTP.TabIndex = 45;
             this.labelNoKTP.Text = "No. KTP :";
             // 
             // textBoxNoKTP
             // 
             this.textBoxNoKTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNoKTP.Location = new System.Drawing.Point(100, 272);
-            this.textBoxNoKTP.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxNoKTP.Location = new System.Drawing.Point(133, 335);
+            this.textBoxNoKTP.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxNoKTP.Name = "textBoxNoKTP";
-            this.textBoxNoKTP.Size = new System.Drawing.Size(325, 24);
+            this.textBoxNoKTP.Size = new System.Drawing.Size(432, 29);
             this.textBoxNoKTP.TabIndex = 46;
             // 
             // buttonOpenFile
@@ -71,44 +74,58 @@
             this.buttonOpenFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(191)))), ((int)(((byte)(10)))));
             this.buttonOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold);
             this.buttonOpenFile.ForeColor = System.Drawing.Color.Firebrick;
-            this.buttonOpenFile.Location = new System.Drawing.Point(176, 203);
+            this.buttonOpenFile.Location = new System.Drawing.Point(64, 251);
+            this.buttonOpenFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonOpenFile.Name = "buttonOpenFile";
-            this.buttonOpenFile.Size = new System.Drawing.Size(118, 31);
+            this.buttonOpenFile.Size = new System.Drawing.Size(157, 38);
             this.buttonOpenFile.TabIndex = 49;
             this.buttonOpenFile.Text = "Open File";
             this.buttonOpenFile.UseVisualStyleBackColor = false;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
             // 
             // buttonEncode
             // 
             this.buttonEncode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(191)))), ((int)(((byte)(10)))));
             this.buttonEncode.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold);
             this.buttonEncode.ForeColor = System.Drawing.Color.Firebrick;
-            this.buttonEncode.Location = new System.Drawing.Point(48, 364);
+            this.buttonEncode.Location = new System.Drawing.Point(64, 448);
+            this.buttonEncode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonEncode.Name = "buttonEncode";
-            this.buttonEncode.Size = new System.Drawing.Size(118, 31);
+            this.buttonEncode.Size = new System.Drawing.Size(157, 38);
             this.buttonEncode.TabIndex = 50;
             this.buttonEncode.Text = "Encode";
             this.buttonEncode.UseVisualStyleBackColor = false;
+            this.buttonEncode.Click += new System.EventHandler(this.buttonEncode_Click);
             // 
             // buttonDecode
             // 
             this.buttonDecode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(191)))), ((int)(((byte)(10)))));
             this.buttonDecode.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold);
             this.buttonDecode.ForeColor = System.Drawing.Color.Firebrick;
-            this.buttonDecode.Location = new System.Drawing.Point(287, 364);
+            this.buttonDecode.Location = new System.Drawing.Point(383, 448);
+            this.buttonDecode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonDecode.Name = "buttonDecode";
-            this.buttonDecode.Size = new System.Drawing.Size(118, 31);
+            this.buttonDecode.Size = new System.Drawing.Size(157, 38);
             this.buttonDecode.TabIndex = 51;
             this.buttonDecode.Text = "Decode";
             this.buttonDecode.UseVisualStyleBackColor = false;
+            this.buttonDecode.Click += new System.EventHandler(this.buttonDecode_Click);
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(228, 262);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(312, 22);
+            this.textBoxFilePath.TabIndex = 52;
             // 
             // UploadKTP
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ISA_MiniTrojan.Properties.Resources.backgroundFix;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.ClientSize = new System.Drawing.Size(603, 554);
+            this.Controls.Add(this.textBoxFilePath);
             this.Controls.Add(this.buttonDecode);
             this.Controls.Add(this.buttonEncode);
             this.Controls.Add(this.buttonOpenFile);
@@ -116,6 +133,7 @@
             this.Controls.Add(this.labelNoKTP);
             this.Controls.Add(this.pictureBoxKtp);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UploadKTP";
             this.Text = "UploadKTP";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKtp)).EndInit();
@@ -131,5 +149,6 @@
         private System.Windows.Forms.Button buttonOpenFile;
         private System.Windows.Forms.Button buttonEncode;
         private System.Windows.Forms.Button buttonDecode;
+        private System.Windows.Forms.TextBox textBoxFilePath;
     }
 }
--- ISA_MiniTrojan/UploadKTP.cs
+++ ISA_MiniTrojan/UploadKTP.cs
@@ -16,5 +16,84 @@ namespace ISA_MiniTrojan
         {
             InitializeComponent();
         }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+            openDialog.InitialDirectory =@"C:";
+
+            if(openDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = openDialog.FileName.ToString();
+                pictureBoxKtp.ImageLocation = textBoxFilePath.Text;
+
+            }
+        }
+
+        private void buttonEncode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(textBoxFilePath.Text);
+
+            for(int i = 0; i < img.Width; i++)
+            {
+                for(int j = 0;  j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if(i < 1 && j > textBoxNoKTP.TextLength)
+                    {
+                        Console.WriteLine("R = [" + i + "][" + j + "] = " + pixel.R);
+                        Console.WriteLine("R = [" + i + "][" + j + "] = " + pixel.G);
+                        Console.WriteLine("B = [" + i + "][" + j + "] = " + pixel.B);
+
+                        char letter = Convert.ToChar(textBoxNoKTP.Text.Substring(j,1));
+                        int value = Convert.ToInt32(letter);
+                        Console.WriteLine("Letter : " + letter + " value : " + pixel.B);
+                        img.SetPixel(i, j, Color.FromArgb(pixel.R, pixel.G, value));
+                    }
+
+                    if(i == img.Width - 1 && j == img.Height - 1)
+                    {
+                        img.SetPixel(1, j, Color.FromArgb(pixel.R,pixel.G, textBoxNoKTP.TextLength));
+                    }
+                }              
+            }
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Image Files (*.png, *.jpg) | *.png; *.jpg";
+            saveFile.InitialDirectory = @"C:";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = saveFile.FileName.ToString();
+                pictureBoxKtp.ImageLocation = textBoxFilePath.Text;
+                img.Save(textBoxFilePath.Text);
+            }
+        }
+
+        private void buttonDecode_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(textBoxFilePath.Text);
+            string message = "";
+            Color lastPixel = img.GetPixel(img.Width - 1, img.Height - 1);
+            int msgLength = lastPixel.B;
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if (i < 1 && j > msgLength)
+                    {
+                        int value = pixel.B;
+                        char c = Convert.ToChar(value);
+                        string letter = System.Text.Encoding.ASCII.GetString(new byte[] {Convert.ToByte(c)});                       
+                        
+                        message += letter;
+                    }
+                }
+            }
+            textBoxNoKTP.Text = message;
+        }
     }
 }
