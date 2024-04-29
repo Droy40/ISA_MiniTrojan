8bf09b3aa7f2a9b29981ba1b83f825236e18e4be
Ferdi0107 <s160722021@student.ubaya.ac.id>
2024-04-29 18:41:22 +0700

    TAMBAH FORM
    FORM UPLOAD KTP by FERDI

---
+++ ISA_MiniTrojan/UploadKTP.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ISA_MiniTrojan
+{
+    partial class UploadKTP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenFile = new Guna.UI2.WinForms.Guna2Button();
+            this.pictureBoxKtp = new System.Windows.Forms.PictureBox();
+            this.labelNoKTP = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKtp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.AutoRoundedCorners = true;
+            this.buttonOpenFile.BackColor = System.Drawing.Color.Transparent;
+            this.buttonOpenFile.BorderColor = System.Drawing.Color.Firebrick;
+            this.buttonOpenFile.BorderRadius = 14;
+            this.buttonOpenFile.BorderThickness = 2;
+            this.buttonOpenFile.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.buttonOpenFile.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.buttonOpenFile.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.buttonOpenFile.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.buttonOpenFile.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(191)))), ((int)(((byte)(10)))));
+            this.buttonOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpenFile.ForeColor = System.Drawing.Color.Firebrick;
+            this.buttonOpenFile.Location = new System.Drawing.Point(166, 202);
+            this.buttonOpenFile.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(118, 31);
+            this.buttonOpenFile.TabIndex = 43;
+            this.buttonOpenFile.Text = "Open File";
+            // 
+            // pictureBoxKtp
+            // 
+            this.pictureBoxKtp.Location = new System.Drawing.Point(48, 12);
+            this.pictureBoxKtp.Name = "pictureBoxKtp";
+            this.pictureBoxKtp.Size = new System.Drawing.Size(357, 185);
+            this.pictureBoxKtp.TabIndex = 44;
+            this.pictureBoxKtp.TabStop = false;
+            // 
+            // labelNoKTP
+            // 
+            this.labelNoKTP.AutoSize = true;
+            this.labelNoKTP.BackColor = System.Drawing.Color.Transparent;
+            this.labelNoKTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNoKTP.Location = new System.Drawing.Point(12, 274);
+            this.labelNoKTP.Name = "labelNoKTP";
+            this.labelNoKTP.Size = new System.Drawing.Size(83, 20);
+            this.labelNoKTP.TabIndex = 45;
+            this.labelNoKTP.Text = "No. KTP :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(100, 272);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(325, 24);
+            this.textBox1.TabIndex = 46;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.AutoRoundedCorners = true;
+            this.guna2Button1.BorderColor = System.Drawing.Color.Firebrick;
+            this.guna2Button1.BorderRadius = 14;
+            this.guna2Button1.BorderThickness = 2;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(191)))), ((int)(((byte)(10)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.Firebrick;
+            this.guna2Button1.Location = new System.Drawing.Point(58, 364);
+            this.guna2Button1.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(118, 31);
+            this.guna2Button1.TabIndex = 47;
+            this.guna2Button1.Text = "Encode";
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.AutoRoundedCorners = true;
+            this.guna2Button2.BorderColor = System.Drawing.Color.Firebrick;
+            this.guna2Button2.BorderRadius = 14;
+            this.guna2Button2.BorderThickness = 2;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(191)))), ((int)(((byte)(10)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.Firebrick;
+            this.guna2Button2.Location = new System.Drawing.Point(274, 364);
+            this.guna2Button2.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(118, 31);
+            this.guna2Button2.TabIndex = 48;
+            this.guna2Button2.Text = "Decode";
+            // 
+            // UploadKTP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ISA_MiniTrojan.Properties.Resources.backgroundFix;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelNoKTP);
+            this.Controls.Add(this.pictureBoxKtp);
+            this.Controls.Add(this.buttonOpenFile);
+            this.DoubleBuffered = true;
+            this.Name = "UploadKTP";
+            this.Text = "UploadKTP";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKtp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Button buttonOpenFile;
+        private System.Windows.Forms.PictureBox pictureBoxKtp;
+        private System.Windows.Forms.Label labelNoKTP;
+        private System.Windows.Forms.TextBox textBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+    }
+}
