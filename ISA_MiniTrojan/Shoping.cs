107021b7af0514ebef784740970294d85017ccdf
Salman Alfarizi <salmanlfaa11@gmail.com>
2024-05-01 16:53:19 +0700

    Keranjang & db.sql

--- ISA_MiniTrojan/Shoping.cs
+++ ISA_MiniTrojan/Shoping.cs
@@ -14,29 +14,45 @@ namespace ISA_MiniTrojan
     public partial class Shoping : Form
     {
         Dashboard formDashboard;
+        List<Product> listOfProducts;
+        Product selectedItem;
         public Shoping()
         {
             InitializeComponent();
-            formDashboard = this.Owner as Dashboard;
         }
 
         private void Shoping_Load(object sender, EventArgs e)
         {
+            formDashboard = this.Owner as Dashboard;
+            listOfProducts = Product.BacaData();
+            comboBoxBarang.DataSource = listOfProducts;
+            comboBoxBarang.DisplayMember = "Name";
 
-            //comboBoxBarang.DataSource = Product.BacaData();
-            //comboBoxBarang.DisplayMember = "Name";
-            //textBoxHarga.Text = Product.BacaData("P.Price", comboBoxBarang.SelectedValue);
 
-            //labelDeskripsi.Text = Product.BacaData("P.Description", comboBoxBarang.SelectedItem.ToString());
-            //labelHargaTotal.Text = (int.Parse(textBoxHarga.Text) * (int)numericUpDownJumlah.Value).ToString();
-            //labelDiskonHarga.Text = "";
-            //labelHargaAkhir.Text = (int.Parse(labelTotal.Text) * int.Parse(labelDiskon.Text)).ToString();
-            //labelTotalSaldo.Text = "";
         }
 
         private void comboBoxBarang_SelectedIndexChanged(object sender, EventArgs e)
         {
+            selectedItem = comboBoxBarang.SelectedItem as Product;   
+            textBoxHarga.Text = selectedItem.Price.ToString();
+            labelDeskripsi.Text = selectedItem.Description.ToString();
+            labelHargaTotal.Text = (int.Parse(textBoxHarga.Text) * (int)numericUpDownJumlah.Value).ToString();
+        }
 
+        private void buttonBuy_Click(object sender, EventArgs e)
+        {
+            Keranjang k = new Keranjang();
+            k.User = formDashboard.user;
+            k.Product = selectedItem;
+            k.Jumlah = (int)numericUpDownJumlah.Value;
+
+            Keranjang.TambahData(k);
+            MessageBox.Show("Product telah berhasil ditambahkan ke keranjang");
+        }
+
+        private void numericUpDownJumlah_ValueChanged(object sender, EventArgs e)
+        {
+            labelHargaTotal.Text = (int.Parse(textBoxHarga.Text) * (int)numericUpDownJumlah.Value).ToString();
         }
     }
 }
