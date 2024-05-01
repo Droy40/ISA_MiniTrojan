f2c87f4f7810d541a2b0d692fa4d115345779cef
Droy40 <dustindelroy@gmail.com>
2024-05-01 14:51:26 +0700

    form invoice

--- ISA_MiniTrojan/Invoice.Designer.cs
+++ ISA_MiniTrojan/Invoice.Designer.cs
@@ -45,9 +45,9 @@
             // 
             this.dataGridViewInvoice.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewInvoice.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ColumnNama,
-            this.ColumnIdUser,
-            this.ColumnIdTransaksi,
+            //this.ColumnNama,
+            this.ColumnId,
+            //this.ColumnIdTransaksi,
             this.ColumnTgl,
             this.ColumnTotal,
             this.btnDetail});
@@ -163,10 +163,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelNoInvoice;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNama;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIdUser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnId;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIdTransaksi;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTgl;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTotal;
         private System.Windows.Forms.DataGridViewButtonColumn btnDetail;
+
     }
 }
